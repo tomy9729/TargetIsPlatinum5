bb4e5b596169059b67c5469eee9a76c1a3edac80
KimHyunsoo <tomy9729@naver.com>
2021-07-24 23:22:33 +0900

    Create 1000번 A+B.java

---
+++ 김현수/01주차/1000번 A+B.java
@@ -0,0 +1,10 @@
+//1000번 A+B.java
+import java.util.Scanner;
+public class AplusB_1000 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a+b);
+	}
+}
