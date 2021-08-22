fda3c79a007a288eb4a455f1f9710a868476d205
KimHyunsoo <tomy9729@naver.com>
2021-08-22 19:59:17 +0900

    Update 14501번 퇴사.java

--- 김현수/06주차/14501번 퇴사.java
+++ 김현수/06주차/14501번 퇴사.java
@@ -4,6 +4,7 @@ package week6;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class 퇴사_14501 {
@@ -16,10 +17,12 @@ public class 퇴사_14501 {
     static int[] P;
     static int answer;
     static int p;
+    static int dp[];
     public static void main(String[] args) throws NumberFormatException, IOException {
 		N = Integer.parseInt(br.readLine());
-		T = new int[N+1];
-		P = new int[N+1];
+		T = new int[N+2];
+		P = new int[N+2];
+		dp = new int[N+2]; //마지막날까지 포함하여 상담이 가능한 경우를 위해 N+1은 0으로 둔다.
 		for(int i=1;i<=N;i++) {
 			st = new StringTokenizer(br.readLine()," ");
 			T[i] = Integer.parseInt(st.nextToken());
@@ -27,9 +30,14 @@ public class 퇴사_14501 {
 		}
 		
 		answer = 0;
-		p=0;
-		plan(1);
-		System.out.println(answer);
+		//p=0;
+		//plan(1);
+		//System.out.println(answer);
+		
+		for(int i=N;i>0;i--) {
+			plandp(i);
+		}
+		System.out.println(dp[1]);
 	}
     
     public static void plan(int day) {
@@ -45,5 +53,22 @@ public class 퇴사_14501 {
     		}
     		if(i<=N)plan(i+1);//상담을 하지 않는 경우
     	}
+    }
+    
+    public static void plandp(int day) {
+    	if(day+T[day]-1 > N) { //현재 상담의 기간이 퇴사일을 넘어가면
+    		if(day+1<=N) {
+    			dp[day]=dp[day+1]; //다음날이 존재하면 다음날로 초기화
+    		}
+    		return;
+    	}
+    	if(day==N) { //마지막날이라면, 위에 안걸리므로 무조건 T[N]=1
+    		dp[day]=P[day];
+    		return;
+    	}
+    	if(day+T[day]-1<=N) {//상담이 가능하다면
+    		dp[day] = Math.max(dp[day+1],P[day]+dp[day+T[day]]);//현재 상담을 하는 것  vs 현재 상담을 하지 않는 것 중 최댓값
+    	}
+		
     }
 }
